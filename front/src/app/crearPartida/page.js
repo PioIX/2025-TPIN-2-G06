8dfd188e513e03658a46f46d523e09dacdb58f49
lucasSimonetti1712 <lsimonetti@pioix.edu.ar>
2025-10-20 10:06:19 -0300

    avance en estilos

--- front/src/app/crearPartida/page.js
+++ front/src/app/crearPartida/page.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import { useSearchParams } from "next/navigation";
-
+import styles from "./crearPartida.module.css";
 
 export default function Prueba() {
   const { socket } = useSocket();
@@ -20,18 +20,15 @@ export default function Prueba() {
     const paramIdUsuario = searchParams.get("idUsuario");
     setIdPersonaje(paramId);
     setIdUsuario(paramIdUsuario);
-
-
   }, []);
   async function manejarPartida(tipo) {
-
     try {
       const res = await fetch("http://localhost:4000/entrarPartida", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           user: idUsuario,
-          roomId:idRoom,
+          roomId: idRoom,
           personaje: idPersonaje,
           tipo: tipo,
         }),
@@ -54,24 +51,36 @@ export default function Prueba() {
 
   return (
     <>
-      {hacer == "elegir" && (
-        <div>
-          <Button onClick={() => manejarPartida("crear")} text="Crear Partida" />
-          <Button onClick={() => setHacer("unirse")} text="Unirse a Partida" />
-        </div>
-      )}
+      <div className={styles.wrapper}>
+        {hacer === "elegir" && (
+          <div className={styles.buttonGroup}>
+            <Button
+              onClick={() => manejarPartida("crear")}
+              text="Crear Partida"
+            />
+            <Button
+              onClick={() => setHacer("unirse")}
+              text="Unirse a Partida"
+            />
+          </div>
+        )}
 
-      {hacer === "unirse" && (
-        <div>
-          <Input
-            placeholder="Coloca el id de la partida"
-            onChange={(e) => setIdRoom(e.target.value)}
-            value={idRoom}
-          />
-          <Button onClick={() => manejarPartida("unirse")} text="Unirse a Partida" />
-          <Button onClick={() => setHacer("elegir")} text="Volver" />
-        </div>
-      )}
+        {hacer === "unirse" && (
+          <div className={styles.buttonGroup}>
+            <Input
+              className={styles.inputStyled}
+              placeholder="Coloca el id de la partida"
+              onChange={(e) => setIdRoom(e.target.value)}
+              value={idRoom}
+            />
+            <Button
+              onClick={() => manejarPartida("unirse")}
+              text="Unirse a Partida"
+            />
+            <Button onClick={() => setHacer("elegir")} text="Volver" />
+          </div>
+        )}
+      </div>
     </>
   );
 }
